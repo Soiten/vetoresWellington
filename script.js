7009e464d79cb032bed34cb48fe54bb6dd1e0945
Renan Araujo Polainas <re.araujo.po@gmail.com>
2025-05-20 20:30:40 -0300

    refactor geral

--- script.js
+++ script.js
@@ -1,63 +1,102 @@
-const QUANT_CIRCULOS = 50;
-
-const raios = [];
-const pos = [];
-const vels = [];
-const grav = new p5.Vector(0, 0.06);
+let quant_bolas = 10;
+let grav = new p5.Vector(0, 0.016);
+let cores = [];
+let raios = [];
+let massas = [];
+let pos = [];
+let vels = [];
 
 function setup() {
   createCanvas(400, 400);
+  noStroke();
   colorMode(HSB, 360, 100, 100);
-
-  //valores iniciais
-  for (let i = 0; i < QUANT_CIRCULOS; i++) {
-    raios.push(random(4, 10));
-    pos.push(createVector(random(width), random(height)));
-    vels.push(createVector(random(-2.5, 2.5), random(-2.5, 2.5)));
+  for (let i = 0; i < quant_bolas; i++) {
+    pos.push(createVector(random(width), random(20)));
+    vels.push(createVector(random(-2, 2), random(-2, 2)));
+    let taxa = random(1, 2);
+    massas.push(taxa * 20);
+    raios.push(taxa * 7);
+    cores.push((taxa - 1) * 330);
   }
 }
 
 function draw() {
-  background(60);
+  background(10);
 
-  const mouseVec = createVector(mouseX, mouseY);
-
-  for (let i = 0; i < QUANT_CIRCULOS; i++) {
-    //fisica
-    vels[i].add(grav);
-    pos[i].add(vels[i]);
-
-    //colisoes
-    if (pos[i].y + raios[i] > height) {
-      pos[i].y = height - raios[i];
-      vels[i].y *= -1;
-    }
-
-    if (pos[i].x + raios[i] > width) {
-      pos[i].x = width - raios[i];
-      vels[i].x *= -1;
-    }
-
-    if (pos[i].x - raios[i] < 0) {
-      pos[i].x = raios[i];
-      vels[i].x *= -1;
-    }
-
-    //drag
-    vels[i].mult(0.997);
-
-    //aplica aceleração na direção do mouse
-    if (mouseIsPressed) {
-      noFill();
-      circle(mouseX, mouseY, 50 * 2);
-      let diferenca = p5.Vector.sub(mouseVec, pos[i]);
-      if (diferenca.mag() < 50) {
-        vels[i].add(diferenca.limit(1.5));
+  for (let i = 0; i < quant_bolas; i++) {
+    for (let j = 0; j < quant_bolas; j++) {
+      if (j == i) continue;
+      const distance = pos[i].dist(pos[j]);
+      if (distance < raios[i] + raios[j]) {
+        [pos[i], vels[i], pos[j], vels[j]] = resolveCollision(
+          pos[i],
+          vels[i],
+          massas[i],
+          raios[i],
+          pos[j],
+          vels[j],
+          massas[j],
+          raios[j],
+          distance
+        );
       }
     }
+    if (pos[i].x < raios[i]) {
+      pos[i].x = raios[i];
+      vels[i].mult(-1, 1);
+    }
+    if (pos[i].y < raios[i]) {
+      pos[i].y = raios[i];
+      vels[i].mult(1, -1);
+    }
+    if (pos[i].x > width - raios[i]) {
+      pos[i].x = width - raios[i];
+      vels[i].mult(-1, 1);
+    }
+    if (pos[i].y > height - raios[i]) {
+      pos[i].y = height - raios[i];
+      vels[i].mult(1, -1);
+    }
 
-    //desenha
-    fill(pos[i].x, 100, 100);
+    vels[i].add(grav);
+    pos[i].add(vels[i]);
+    fill(cores[i], 100, 100);
     circle(pos[i].x, pos[i].y, raios[i] * 2);
   }
+}
+
+function resolveCollision(p1, v1, m1, r1, p2, v2, m2, r2, distance) {
+  let n = p5.Vector.sub(p1, p2).div(distance);
+  let p = p5.Vector.sub(p5.Vector.mult(v1, n), p5.Vector.mult(v2, n))
+    .mult(2)
+    .div(m1 + m2);
+  v1 = p5.Vector.sub(v1, p5.Vector.mult(n, p5.Vector.mult(p, m1)));
+  v2 = p5.Vector.add(v2, p5.Vector.mult(n, p5.Vector.mult(p, m2)));
+
+  let mid = p5.Vector.sub(p2, p1).setMag(r1).add(p1).add(p1.copy().sub(p2).setMag(r2).add(p2)).mult(0.5);
+
+  p1 = mid.copy().add(
+    p1
+      .copy()
+      .sub(mid)
+      .setMag(r1 * 1.01)
+  );
+  p2 = mid.copy().add(
+    p2
+      .copy()
+      .sub(mid)
+      .setMag(r2 * 1.01)
+  );
+
+  return [p1, v1, p2, v2];
+}
+
+function mouseClicked() {
+  quant_bolas++;
+  pos.push(createVector(mouseX, mouseY));
+  vels.push(createVector(random(-2, 2), random(-2, 2)));
+  let taxa = random(1, 2);
+  massas.push(taxa * 20);
+  raios.push(taxa * 7);
+  cores.push((taxa - 1) * 330);
 }
